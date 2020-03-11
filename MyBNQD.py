742dd4239c24358ad0551548086f0bb93f39ef76
Melle Starke <mellestarke@gmail.com>
2020-03-11 18:28:23 +0100

    Got the discontinuous model to """work""", though the variance for after the intervention point is super high. I also have to find a more elegant way of dealing with the differences in data shapes between the continuous and discontinuous models

--- MyBNQD.py
+++ MyBNQD.py
@@ -140,24 +140,92 @@ class ContinuousModel(BNQDModel):
 
 class DiscontinuousModel(BNQDModel):
 
-    def __init__(self, data: DiscontinuousData, kernel: Kernel, likelihood: Likelihood, intv_point: float,
-                 labels: Union[List, Callable], shared_params: bool = False,
-                 mean_function: Optional[MeanFunction] = None, num_latent: int = 1):
+    def __init__(self, data: DiscontinuousData, kernel: Kernel, likelihood: Likelihood, intv_point,
+                 shared_params: bool = False, mean_function: Optional[MeanFunction] = None, num_latent: int = 1):
+        # TODO: allow the user to set this in an elegant way
+        # Maximum optimizer iterations
+        self.MAX_OPT_ITER = 1000
+
         super().__init__(data, kernel, likelihood, mean_function, num_latent)
-        raise NotImplementedError
+        print("len data[0][0] shape: {}".format(len(data[0][0].shape)))
+        # TODO: check if this data set shape stuff actually works
+        if len(data[0][0].shape) == 1:  # Checks if the input data is of rank 1 (i.e. vector)
+            # Converts the input data of shape (N) to shape (N,1), to comply with tensorflow.matmul requirements
+            # The data is split between control and intervention data
+            (x_c, y_c), (x_i, y_i) = data
+            self.data = ((x_c[:, None], y_c[:, None]), (x_i[:, None], y_i[:, None]))
+
+        else:  # Assigns without changing the dimensions of the input data
+            self.data = data
+
+        self.shared_params = shared_params
+        self.intv_point = intv_point
+        self.N = data[0][0].shape[0] + data[1][0].shape[0] # nr. of data points
+        data_c, data_i = self.data
+        self.m_c = GPR(data_c, self.kernel)
+        self.m_i = GPR(data_i, self.kernel)
 
     def train(self, optimizer=optimizers.Scipy(), verbose=True):
-        raise NotImplementedError
+        # function that the optimizer aims to minimize
+        closure = lambda: -(self.m_c.log_marginal_likelihood() + self.m_i.log_marginal_likelihood())
+
+        optimizer.minimize(closure, self.m_c.trainable_parameters + self.m_i.trainable_parameters,
+                           options=(dict(maxiter=self.MAX_OPT_ITER)))
+        if verbose:
+            gf.utilities.print_summary(self.m_c)
+            gf.utilities.print_summary(self.m_i)
+
+        self.isTrained = True
+        self.BICScore = None  # Ensures the BIC score is updated when trying to access it after training the model
 
     def log_likelihood(self, *args, **kwargs) -> tf.Tensor:
-        raise NotImplementedError
+        if not self.isTrained:
+            # Prints a warning if the model hasn't been trained.
+            # Then trains it with the default optimizer
+            msg = "The model hasn't been trained yet. It will be trained using the default optimizer.\n" \
+                  "If you wish to use a different optimizer, please use the DiscontinuousModel.train(optimizer) function"
+            warnings.warn(msg, category=UserWarning)
+            self.train()
+
+        if not self.BICScore:  # Means: if self.BICScore == None
+            # Parameters are represented as tuple, for documentation see gpf.Module
+            # Will only give the number of parameters of the control model if the parameters are shared
+            k = len(self.m_c.parameters) + len(self.m_i.parameters) * (not self.shared_params)
+            L = self.m_c.log_likelihood() + self.m_i.log_likelihood()
+            BIC = L - k / 2 * np.log(self.N)  # BIC score is the likelihood with penalization for the nr. of parameters
+            self.BICScore = BIC
+        return self.BICScore
 
     def predict_f(self, predict_at: DataPoint, full_cov: bool = False,
                   full_output_cov: bool = False) -> MeanAndVariance:
-        raise NotImplementedError
+        print("predict_at shape: {}".format(predict_at.shape))
+        predict_c = predict_at[predict_at[:, 0] < self.intv_point, :]
+        predict_i = predict_at[predict_at[:, 0] < self.intv_point, :]
+        print("predict_c shape: {}".format(predict_c.shape))
+        mean_c, var_c = self.m_c.predict_f(predict_c, full_cov, full_output_cov)
+        mean_i, var_i = self.m_i.predict_f(predict_i, full_cov, full_output_cov)
+        return tf.concat([mean_c, mean_i], 0), tf.concat([var_c, var_i], 0)
 
-    def plot(self, n_samples: int = 100):
-        raise NotImplementedError
+    def plot(self, n_samples: int = 100, verbose: bool = True):
+        # finds minimum and maximum x values
+        x_vals = self.data[0][0] + self.data[1][0]
+        min_x, max_x = (min(x_vals[:, 0]), max(x_vals[:, 0]))
+
+        # creates n_samples data points between min_x and max_x
+        x_samples = np.linspace(min_x, max_x, n_samples)
+
+        # finds the mean and variance for each element in x_samples
+        mean, var = self.predict_f(x_samples[:, None])
+        if verbose:
+            print("min_x: {}\tshape: {}\nmax_x: {}\tshape: {}\nx_samples shape: {}\nmean shape: {}\nvar shape: {}"
+                  .format(min_x, min_x.shape, max_x, max_x.shape, x_samples.shape, mean.shape, var.shape))
+
+        # Plots the mean function predicted by the GP
+        plt.plot(x_samples, mean[:, 0], c='blue', label='$M_c$')
+        # Plots the 95% confidence interval
+        # TODO: figure out why the variance is SO BIG AFTER THE INTERVENTION POINT
+        plt.fill_between(x_samples, mean[:, 0] - 1.96 * np.sqrt(var[:, 0]), mean[:, 0] + 1.96 * np.sqrt(var[:, 0]),
+                         color='blue', alpha=0.2)
 
 
 ######################
@@ -173,8 +241,14 @@ f = 0.8 * np.sin(x) + 0.2 * x ** 2 + 0.2 * np.cos(x / 4) + 1.0 * (x > b)
 sigma = 0.65
 y = np.random.normal(f, sigma, size=n)
 print(sigma)
-plt.figure()
 
+ip = 0
+x_c = x[x < ip]
+y_c = y[x < ip]
+x_i = x[x >= ip]
+y_i = y[x >= ip]
+
+plt.figure()
 if True:  # disables / enables plotting of the data points and generative function
     plt.plot(x[x <= b], f[x <= b], label='True f', c='k')
     plt.plot(x[x >= b], f[x >= b], c='k')
@@ -185,14 +259,20 @@ if True:  # disables / enables plotting of the data points and generative functi
 ###### Kernel Options #####
 
 #k = Linear() + Constant() # "Linear" kernel
-#k = Exponential()
-k = SquaredExponential()
+k = Exponential()
+#k = SquaredExponential()
 #k = Periodic(SquaredExponential())
 #k = Cosine() + Constant()
 
-cm = ContinuousModel((x, y), k, Gaussian())
-print("trainable parameters: {}".format(cm.m.trainable_parameters))
-cm.train()
-cm.plot()
+# Use the continuous model or discontinuous one
+useContinuous = 0
+
+m = None
+if useContinuous:
+    m = ContinuousModel((x, y), k, Gaussian())
+else:
+    m = DiscontinuousModel(((x_c, y_c), (x_i, y_i)), k, Gaussian(), ip)
+m.train()
+m.plot()
 plt.show()
-print("\ncontinuous model:\n\tBIC score: {}\n\tlog likelihood: {}".format(cm.log_likelihood(), cm.m.log_likelihood()))
+#print("\ncontinuous model:\n\tBIC score: {}\n\tlog likelihood: {}".format(m.log_likelihood(), m.m.log_likelihood()))
