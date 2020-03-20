Fixed the issue with the small variance during plotting, renamed and moved some files, renamed some fields, made the BNQDAnalysis object, tidied up the log_likelihood() and log_marginal_likelihood functions, added documentation until DiscontinuousModel.predict_f() function, changed the DiscontinuousData objects into lists instead of tuples, made a separate file for testing called bnqdflow_testing_facility.py, added full support for sharing and not sharing hyper parameters, made the use of (trainable_)variables/parameters more in line with GPflow models

example_implementations/bnqdflow_tests_max.py
@@ -15,6 +15,7 @@ from scipy.linalg import block_diag
 from typing import Optional, Tuple
 
 from gpflow.models import GPModel
+from gpflow.models.model import MeanAndVariance
 from gpflow.kernels import Kernel
 from gpflow.logdensities import multivariate_normal
 from gpflow.mean_functions import MeanFunction
@@ -74,7 +75,7 @@ class DGPR(GPModel):
             log_prob += multivariate_normal(y, m, L)
         return tf.reduce_sum(log_prob)
 
-    def predict_f(self, predict_at: tf.Tensor, full_cov: bool = False, full_output_cov: bool = False):
+    def predict_f(self, predict_at: tf.Tensor, full_cov: bool = False, full_output_cov: bool = False) -> MeanAndVariance:
         r"""
         todo
         
@@ -119,7 +120,7 @@ b = 0.0
 n = 100
 x = np.linspace(-3, 3, n)
 f = 0.8*np.sin(x) + 0.2*x**2 + 0.2*np.cos(x/4) + 1.0*(x>b)
-sigma = 1
+sigma = 0.65
 y = np.random.normal(f, sigma, size=n)
 
 plt.figure()

test_scripts/playground.py
@@ -1,9 +1,7 @@
 #import BNQD
 #import gpflow as gpf
 import numpy as np
-import util
-import matplotlib.pyplot as plt
-#import tensorflow as tf
+import tensorflow as tf
 '''
 xs, ys = util.linear_dummy_data()
 cm = BNQD.ContinuousModel(xs, ys, gpf.kernels.Linear())
@@ -20,6 +18,21 @@ f = 0.8*np.sin(x) + 0.2*x**2 + 0.2*np.cos(x/4) + 1.0*(x>b)
 sigma = np.sqrt(1)
 y = np.random.normal(f, sigma, size=n)
 
-print("x: {}, shape: {}\nx[:,None]: {}, shape: {}".format(x, x.shape, x[:,None], x[:,None].shape))
+xc = tf.Variable([1, 2, 3])
+xi = tf.Variable([4, 5, 6])
+yc = tf.Variable([10, 20, 30])
+yi = tf.Variable([40, 50, 60])
+dc = (xc, yc)
+di = (xi, yi)
+
+res = tf.Variable([[], []])
+print(res)
+for d in [dc, di]:
+    res = tf.concat([res, d], 1)
+
+sum = tf.reduce_sum([dc, di], 0)
+
+print(res)
+print(sum)
 
 